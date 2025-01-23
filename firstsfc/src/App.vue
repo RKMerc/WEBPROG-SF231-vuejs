057cdc8fdb461b53494da67ce35dac2bb879d665
RKMerc <ramercado3@student.apc.edu.ph>
2025-01-23 15:19:26 +0800

    updated app.vue

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,22 +1,11 @@
-<script>
-</script>
+<script></script>
 
 <template>
-  <h1>Food</h1>
-  <food-item/><br>
-  <food-item2/><br>
-  <food-item/><br>
+   <h1>Food</h1>
+  <food-item/> <br>
+  <food-item2/> <br>
+  <food-item/>  <br>
+  <food-item2/> <br>
 </template>
 
-<style>
-  #app > div {
-    border: dashed black 1px;
-    display: inline-block;
-    margin: 10px;
-    padding: 10px;
-    background-color: lightgreen;
-  }
-  #app > div:hover {
-    cursor: pointer;
-  }
-</style>
+<style></style>
