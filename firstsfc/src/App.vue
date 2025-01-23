7f06dbb1db49c6367d42d723f81842098111dcec
RKMerc <ramercado3@student.apc.edu.ph>
2025-01-23 14:54:27 +0800

    updated app.vue, main.js, and fooditem.vue

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -3,7 +3,21 @@
 
 <template>
   <h1>Hello WEBPROG SF231!</h1>
+  <h1>Food</h1>
+  <food-item/>
+  <food-item/>
+  <food-item/>
 </template>
 
 <style>
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+  #app > div:hover {
+    cursor: pointer;
+  }
 </style>
