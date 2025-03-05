6f5167933c80c0bcaec8ea440d75a21233187e06
RKMerc <ramercado3@student.apc.edu.ph>
2025-03-05 08:03:26 +0000

    updated personalprofile.vue

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -3,43 +3,34 @@
 <h1>Personal Profile Web page</h1>
 
 <table>
-  <tr>
-    <th>Topic</th>
-    <th>Information</th>
-  </tr>
-  <tr>
-    <td>About Me:</td>
-    <td>I am Rafael Kenneth A. Mercado. I am currently studying in Asia Pacific College with an interest in game design and game development.</td>
-  </tr>
-  <tr>
-    <td>Education / Achievements</td>
-    <td>I am currently taking an undergraduate degree as part of my college education and I have also completed my education in high school (Junior/Senior).</td>
-  </tr>
-  <tr>
-    <td>Course</td>
-    <td>The undergraduate degree that I am taking is BSCS (Bachelor of Science in Computer Science) that is offered by APC.</td>
-  </tr>
-  <tr>
-    <td>IT Experience</td>
-    <td>I have done some light coding in the past both as a hobby/personal interest as well as some moderate coding for one of my research papers in the past.</td>
-  </tr>
-  <tr>
-    <td>Hobbies & Interests</td>
-    <td>One of my main hobbies is to read and play video games. Reading provides me with much entertainment for times when I am either bored or have completed my tasks. 
-      Gaming helps relax my mind as well as de-stress from work or studies and helps provide me with some insight as to how other games implement unique systems and game
-      mechanics into their games. This experience is valuable to me both for social and professional improvements as I aspire to be a game developer as well as allow me to
-      see what is both popular and engaging for different kinds of people with regards to games.
-    </td>
-  </tr>
-  <tr>
-    <td>Goals In Life / Dream</td>
-    <td>As mentioned before, I aspire to be a game developer. This is one of the steps that will help me achieve one of my main goals which is to setup my own game
-    development studio. This will allow me to have freedom to develop games as well as to have a gathering of like-minded people with regards to my goals. Another
-    goal of mine is to setup my own internet cafe. Lately, I developed an interest in building computer systems and that I find it really fascinating how each 
-    component plays a role in the entire build process, be it budget related or compatibility related. I also hold a fond memory of internet cafes as this was one
-    of the times earlier in my life where I really enjoyed playing computer games as we did not own our own computer back then. Attached below is one of the <a href = "https://www.facebook.com/tnccebu/">examples</a>
-    of an internet cafe that I want to build in the future.</td>
-  </tr>
+  <thead>
+    <tr>
+      <th>Category</th>
+      <th>Details</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Personal Information</td>
+      <td>Rafael Kenneth A. Mercado, student at Asia Pacific College, interested in game design and development.</td>
+    </tr>
+    <tr>
+      <td>Education</td>
+      <td>Bachelor of Science in Computer Science (BSCS) at APC; completed high school (Junior/Senior).</td>
+    </tr>
+    <tr>
+      <td>Technical Skills</td>
+      <td>Experience with light coding (hobby/personal projects) and moderate coding for research papers.</td>
+    </tr>
+    <tr>
+      <td>Interests & Hobbies</td>
+      <td>Reading, video games (analyzing game mechanics, social/professional insights), game development aspirations.</td>
+    </tr>
+    <tr>
+      <td>Career Goals</td>
+      <td>Establish a game development studio and an internet cafe (inspired by <a href="https://www.facebook.com/tnccebu/">TNC Cebu</a>), reflecting an interest in PC building.</td>
+    </tr>
+  </tbody>
 </table>
 
 <p>Some of the game genres that I have in mind are:</p>
