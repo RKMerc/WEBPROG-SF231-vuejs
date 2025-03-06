2d86dc2ea923ea7e42f89fa63e948a485b3f8748
RKMerc <ramercado3@student.apc.edu.ph>
2025-03-06 13:28:27 +0800

    updated personal profile

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -1,46 +1,58 @@
 
 <template>
-<h1>Personal Profile Web page</h1>
+<head>
+  <title>My first Vue page</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/flexbox-grid.css?version=0.1">
+  <link rel="stylesheet" href="css/pink.css?version=0.1">
+  <link rel="stylesheet" href="css/images.css?version=0.2">
+  <link rel="stylesheet" href="css/shopping.css?version=0.1">
+</head>
+<body>
 
-<table>
-  <tr>
-    <th>Topic</th>
-    <th>Information</th>
-  </tr>
-  <tr>
-    <td>About Me:</td>
-    <td>I am Rafael Kenneth A. Mercado. I am currently studying in Asia Pacific College with an interest in game design and game development.</td>
-  </tr>
-  <tr>
-    <td>Education / Achievements</td>
-    <td>I am currently taking an undergraduate degree as part of my college education and I have also completed my education in high school (Junior/Senior).</td>
-  </tr>
-  <tr>
-    <td>Course</td>
-    <td>The undergraduate degree that I am taking is BSCS (Bachelor of Science in Computer Science) that is offered by APC.</td>
-  </tr>
-  <tr>
-    <td>IT Experience</td>
-    <td>I have done some light coding in the past both as a hobby/personal interest as well as some moderate coding for one of my research papers in the past.</td>
-  </tr>
-  <tr>
-    <td>Hobbies & Interests</td>
-    <td>One of my main hobbies is to read and play video games. Reading provides me with much entertainment for times when I am either bored or have completed my tasks. 
-      Gaming helps relax my mind as well as de-stress from work or studies and helps provide me with some insight as to how other games implement unique systems and game
-      mechanics into their games. This experience is valuable to me both for social and professional improvements as I aspire to be a game developer as well as allow me to
-      see what is both popular and engaging for different kinds of people with regards to games.
-    </td>
-  </tr>
-  <tr>
-    <td>Goals In Life / Dream</td>
-    <td>As mentioned before, I aspire to be a game developer. This is one of the steps that will help me achieve one of my main goals which is to setup my own game
-    development studio. This will allow me to have freedom to develop games as well as to have a gathering of like-minded people with regards to my goals. Another
-    goal of mine is to setup my own internet cafe. Lately, I developed an interest in building computer systems and that I find it really fascinating how each 
-    component plays a role in the entire build process, be it budget related or compatibility related. I also hold a fond memory of internet cafes as this was one
-    of the times earlier in my life where I really enjoyed playing computer games as we did not own our own computer back then. Attached below is one of the <a href = "https://www.facebook.com/tnccebu/">examples</a>
-    of an internet cafe that I want to build in the future.</td>
-  </tr>
-</table>
+<main class="grid-container">
+  <article class="flex-container">
+   <h2>About Me:</h2>
+     <p>
+      I am Rafael Kenneth A. Mercado. I am currently studying in Asia Pacific College with an interest in game design and game development.
+     </p>
+  </article>
+ 
+  <article class="flex-container">
+   <h2>Education / Achievements: </h2>
+    <p>
+     I am currently taking an undergraduate degree as part of my college education and I have also completed my education in high school (Junior/Senior).
+    </p>
+  </article>
+ 
+  <article class="flex-container">
+   <h2>Course: </h2>
+     <p>
+      The undergraduate degree that I am taking is BSCS (Bachelor of Science in Computer Science) that is offered by APC.
+     </p>
+  </article>
+ 
+  <article class="flex-container">
+   <h2>IT Experience: </h2>
+     <p>
+      I have done some light coding in the past both as a hobby/personal interest as well as some moderate coding for one of my research papers in the past.
+     </p>
+  </article>
+ 
+  <article class="flex-container">
+   <h2>Hobbies & Interests: </h2>
+     <p>
+      One of my main hobbies is to read and play video games. Reading provides me with much entertainment for times when I am either bored or have completed my tasks. Gaming helps relax my mind as well as de-stress from work or studies and helps provide me with some insight as to how other games implement unique systems and game mechanics into their games. This experience is valuable to me both for social and professional improvements as I aspire to be a game developer as well as allow me to see what is both popular and engaging for different kinds of people with regards to games.
+     </p>
+  </article>
+ 
+  <article class="flex-container">
+   <h2>Goals in Life / Dream: </h2>
+     <p>
+      My main goal is to become a game developer and eventually run my own game development studio in the future. Another dream of mine is to set up my own internet cafe as it was one of the main influences as to why I want to become a game developer.
+     </p>
+  </article>
+</main>
 
 <p>Some of the game genres that I have in mind are:</p>
 <ol>
@@ -53,4 +65,36 @@
 </template>
     
 <script></script>
-<style></style>
+<style>
+body {
+    width: 100%;
+    height: 100%;
+    background-position: center;
+    background-size: cover;
+    background-image: url(https://png.pngtree.com/thumb_back/fh260/background/20211025/pngtree-bottom-console-image_913604.png);
+    background-attachment: fixed;
+}
+
+article {
+    margin: 1rem 0;
+    font-family: 'Courier New', Courier, monospace;
+}
+
+button {
+    padding: 10px 20px;
+    background-color: #ffffff;
+    border: none;
+    color: white;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    cursor: pointer;
+    transition: background-color 0.3s ease, color 0.3s ease; /* Smooth transition */
+  }
+  
+  button.hovered {
+    background-color: #45a049; /* Darker green on hover */
+      color: black;
+  }
+</style>
